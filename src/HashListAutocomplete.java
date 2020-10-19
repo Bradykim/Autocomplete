51c3e944514f23dc1dd1a71c156d0b9d3df15ab8
Brady Kim <bnk12@duke.edu>
2020-10-19 18:54:40 -0400

    P5 AutoComplete Final 7

--- src/HashListAutocomplete.java
+++ src/HashListAutocomplete.java
@@ -18,7 +18,9 @@ public class HashListAutocomplete implements Autocompletor
     }
     public void initialize(String[] terms, double[] weights)
     {
-
+        if(!myMap.isEmpty()){
+            myMap.clear();
+        }
         myMap=new HashMap<String, List<Term>>();
         for(int i =0; i< terms.length;i++)
         {
